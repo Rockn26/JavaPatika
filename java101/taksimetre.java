c7586f993ae552fe9d428174d66cad43096ab6b3
Rockn26 <irfanank26@gmail.com>
2023-09-11 17:58:31 +0300

    Taksimetre
    Taksimetre programı yaptık.

---
+++ java101/taksimetre.java
@@ -0,0 +1,24 @@
+package java101;
+import java.util.Scanner;
+
+public class taksimetre {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        int km;
+        double perKm = 2.20, total,  open = 10;
+
+        System.out.print("Gideceğiniz mesafeyi km cinsinden giriniz : ");
+        km = inp.nextInt();
+
+        total = km*perKm;
+
+        // tutarı 0 olarak alırsak ona 10 tl açılış ücreti eklememiz gerekiyor.
+        total += open;
+
+        total = (total<20) ? 20 : total;
+
+        System.out.println("Ödeyeceğiniz tutar : " + total + " Tl");
+
+    }
+}
