5683fc19dc0b080d39abed500f8d0c822215e7d4
Rockn26 <irfanank26@gmail.com>
2023-09-13 18:26:14 +0300

    Harmonik Sayı
    Harmonik sayıyı öğrendik. Formülü for döngüsünün içine yazdık

---
+++ java101/harmonik_sayilar.java
@@ -0,0 +1,20 @@
+package java101;
+import java.util.Scanner;
+
+public class harmonik_sayilar {
+    public static void main(String[] args) {
+
+        int n;
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Bir sayı giriniz : ");
+        n = inp.nextInt();
+        double result = 0.0;
+
+        for (double i = 1; i <= n; i++) {
+            result += (1/i);
+        }
+
+        System.out.println("Sonuç: " + result);
+
+    }
+}
