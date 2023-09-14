27337d31b4710ea694f08c81ce2a43da5a309b85
Rockn26 <irfanank26@gmail.com>
2023-09-14 21:48:07 +0300

    Tip dönüşümleri
    Tip dönüşümleri hakkında bilgilendirme

---
+++ java101/tip_donusumleri.java
@@ -0,0 +1,20 @@
+package java101;
+import java.util.Scanner;
+
+public class tip_donusumleri {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        double sayiDouble = 10.6;
+        int sayiInt = (int)sayiDouble;
+
+        int sayi2 = 8;
+        double sayiDouble2 = sayi2;
+
+        System.out.println(sayiDouble);
+        System.out.println(sayiInt);
+        System.out.println("----------------");
+        System.out.println(sayi2);
+        System.out.println(sayiDouble2);
+    }
+}
