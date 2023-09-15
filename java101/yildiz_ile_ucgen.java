a4ac77f276b5317bc4f3ef0c8eb796272df99099
Rockn26 <irfanank26@gmail.com>
2023-09-15 15:17:15 +0300

    Yıldız ile Ucgen
    Yorum satırlarını ekledim.

--- java101/yildiz_ile_ucgen.java
+++ java101/yildiz_ile_ucgen.java
@@ -9,14 +9,11 @@ public class yildiz_ile_ucgen {
         System.out.print("Basamak sayısını giriniz : ");
         n = inp.nextInt();
 
-        // burada aşağıya ln komutuyla beraber 6 satır indik.
-        for (int i = 1; i <= 6; i++) {
-            // burada ln komutunu kaldırdık ve her satıra boşluk ekledik.
-            for (int j = 1; j <= (n-i); j++) {
+        for (int i = 1; i <= n; i++) { // girilen n sayısı kadar satır çıkardık; n =5 => 1-2-3-4-5 örnek olarak gösterdim.
+            for (int j = 1; j <= (n-i); j++) { // girilen n-i sayısı kadar boşluk komutu verdik.
                 System.out.print(" ");
             }
-            // burada ln kullanarak satıra yıldızları bastırıyoruz.
-            for (int k = 1; k <= ((2*i)-1); k++) {
+            for (int k = 1; k <= ((2*i)-1); k++) { // 2i - 1 kadar yıldız bastık.
                 System.out.print("*");
             }
             // en son boş bir ln koyuyoruz, bunu yapmazsak yıldızlar alt satıra inmez.
