efaabde5e856599c692e93da0a06ed3812b768c9
Rockn26 <irfanank26@gmail.com>
2023-09-15 15:10:31 +0300

    Ters Ucgen
    Ters ücgen yorum satılarıl

--- java101/ters_ucgen.java
+++ java101/ters_ucgen.java
@@ -3,19 +3,17 @@ import java.util.Scanner;
 
 public class ters_ucgen {
     public static void main(String[] args) {
-        int n = 10;
+        int n;
         Scanner inp = new Scanner(System.in);
         System.out.print("Basamak sayısını giriniz : ");
         n = inp.nextInt();
 
-        // burada satırımızı 10 dan 1 e inecek şekilde sıraladık.
-        for (int i = n; i>= 1; i--) {
-            // boşlukları oluşturduk
-            for (int k = 1; k <= n - i; k++) {
+
+        for (int i = n; i>= n; i--) { // burada n sayısı kadar satır indik. Örn: n = 5 ; 5-4-3-2-1
+            for (int k = 1; k <= n - i; k++) { // burada n sayısının 1 eksiği kadar boşluk komutu verdik.
                 System.out.print(" ");
             }
-            // yıldızları koyduk.
-            for (int j = 1; j <= (2 * i) - 1; j++){
+            for (int j = 1; j <= (2 * i) - 1; j++){ // burada 2n*1 -1 kadar yıldız bıraktık.
                 System.out.print("*");
             }
             System.out.println();
