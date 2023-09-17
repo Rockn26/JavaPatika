bcc58d357e0082875b81feebf154adb6a88833c8
Rockn26 <irfanank26@gmail.com>
2023-09-17 23:18:45 +0300

    Uslu Sayi Hesabı
    Recursive method kullanarak üslü sayı hesabı

--- week2/uslu_sayi_recursive.java
+++ week2/uslu_sayi_recursive.java
@@ -2,26 +2,25 @@ package week2;
 import java.util.Scanner;
 
 public class uslu_sayi_recursive {
-    static int usluSayi() {
+    static int usHesabı(int tabanSayisi, int usSayisi) {
+        if (usSayisi == 0) {
+            return 1;
+        } else {
+            return tabanSayisi * usHesabı(tabanSayisi, usSayisi-1);
+        }
+    }
+
+    public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
         System.out.print("Taban değerini giriniz : ");
         int tabanSayisi = inp.nextInt();
-        System.out.print("Üs değerini girniz : ");
+        System.out.print("Üs değerini giriniz : ");
         int usSayisi = inp.nextInt();
 
-        int result = 1;
-
-        for (int i = 1; i <= usSayisi; i++) {
-
-            result *= tabanSayisi;
-        }
-
-        return result;
+        System.out.println("Sonuç : " + usHesabı(tabanSayisi,usSayisi));
 
 
     }
-    public static void main(String[] args) {
-        System.out.println("Sonuç : " + usluSayi());
 
-    }
+
 }
