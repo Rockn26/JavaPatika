75598e6365b52ef367e23b7457a90ddbea5fe6eb
Rockn26 <irfanank26@gmail.com>
2023-09-18 16:17:11 +0300

    Recursive öğreniyorum
    Konuyu pekiştirmek için toplama örneği

--- week2/ornek.java
+++ week2/ornek.java
@@ -3,28 +3,21 @@ package week2;
 import java.util.Scanner;
 
 public class ornek {
-
-    static boolean asalMi() {
+    public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
         System.out.print("Bir sayı giriniz : ");
-        int sayi = inp.nextInt();
-        boolean durum = true;
+        int input = inp.nextInt();
+        int sonuc = topla(input);
+        System.out.println(sonuc);
 
-        for (int i = 2; i < sayi; i++) {
-            if (sayi % i == 0) {
-                durum = false;
-                break;
-            }
-        }
-        return durum;
+
     }
 
-    public static void main(String[] args) {
-        boolean durum = asalMi();
-        if (durum == true) {
-            System.out.println("Sayı asaldır.");
+    private static int topla(int input) {
+        if (input == 1){
+            return 1;
         } else {
-            System.out.println("Sayı asal değildir");
+            return input + topla(input-1);
         }
     }
 
