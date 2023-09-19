12f5a09ed7288fec96811a794a1aef253015c1db
Rockn26 <irfanank26@gmail.com>
2023-09-19 15:39:34 +0300

    Desen
    Desen Recursive

--- week2/desen_recursive.java
+++ week2/desen_recursive.java
@@ -4,25 +4,28 @@ import java.util.Scanner;
 
 public class desen_recursive {
 
-    static void pattern(int number) {
-        // number 0 veya 0 dan küçük hale gelirse number değeri ekrana yazılır ve return işlemiyle fonksiyon sonlanır
-        if (number<= 0) {
-            System.out.print(number + " ");
-            return;
+    static int pattern(int number, int temp, boolean stop) {
+        System.out.print(number + " ");
+        if (number < 0 || number == 0 || stop == false) {
+            stop = false;
+            if (number != temp) {
+                return pattern(number + 5, temp, stop);
+            } else {
+                return 0;
+            }
+        } else {
+            return pattern(number - 5, temp, stop);
         }
-        System.out.print(number + " ");
-        pattern(number-5);
-        System.out.print(number + " ");
-
-        // başlangıç sayısından başlayacak ve negatif veya 0 olana kadar devam edecek. Ardından kendi sayısına kadar +5
-        // eklenecek. 
     }
 
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
-        System.out.print("N sayısı : ");
-        int number = inp.nextInt();
+        int number, temp;
+        System.out.print("Number sayısını giriniz :  : ");
+        number = inp.nextInt();
+        temp = number;
 
-        pattern(number);
+        pattern(number,temp,true);
+
     }
 }
