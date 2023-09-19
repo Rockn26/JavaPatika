419adf2c96a3892c587e421f19cf2da087823431
Rockn26 <irfanank26@gmail.com>
2023-09-19 13:48:37 +0300

    Asal sayi
    Asal sayi recursive

--- week2/asal_sayi_recursive.java
+++ week2/asal_sayi_recursive.java
@@ -3,30 +3,34 @@ package week2;
 import java.util.Scanner;
 
 public class asal_sayi_recursive {
-    static boolean asalMi(int sayi, int bolen){
-
-
-        if (sayi <= 2) {
-            return sayi == 2;
+    static boolean asalMi(int sayi, int bolen) {
+        if (sayi < 2) {
+            return false;
+        }
+        if (bolen == 1) {
+            return true;
         }
 
         if (sayi % bolen == 0) {
             return false;
         }
-        if (bolen * bolen > sayi) {
-            return true;
-        }
-        return asalMi(sayi, bolen+1);
+        return asalMi(sayi, bolen - 1);
+
+
     }
+
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
         System.out.print("Bir sayi giriniz : ");
         int sayi = inp.nextInt();
 
-        if (asalMi(sayi,2)){
-            System.out.println(sayi + " asal sayıdır.");
+        if (asalMi(sayi, sayi - 1)) {
+            System.out.println(sayi + " asaldır.");
         } else {
-            System.out.println(sayi + " asal sayı değildir.");
+            System.out.println(sayi + " asal değildir.");
+
+
         }
     }
+
 }
