1e8a2b79d8fa3cf97ed9ef761e486c9d75f99822
Rockn26 <irfanank26@gmail.com>
2023-09-20 13:21:54 +0300

    Boks Maci
    Boks Maçı - Sınıflar

---
+++ week2/Boks_Maci/src/Fighther.java
@@ -0,0 +1,7 @@
+package week2.Boks_Maci.src;
+
+public class Fighther {
+    String name;
+    String dammage;
+
+}
---
+++ week2/Boks_Maci/src/Main.java
@@ -0,0 +1,6 @@
+package week2.Boks_Maci.src;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("a");
+    }
+}
