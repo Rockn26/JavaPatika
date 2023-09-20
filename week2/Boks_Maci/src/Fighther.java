1f24efcf722bc18e62f574a708b9c9b1333be6eb
Rockn26 <irfanank26@gmail.com>
2023-09-20 14:43:58 +0300

    Boks Maci
    Boks Maci Sınıflar

--- week2/Boks_Maci/src/Fighther.java
+++ week2/Boks_Maci/src/Fighther.java
@@ -2,6 +2,48 @@ package week2.Boks_Maci.src;
 
 public class Fighther {
     String name;
-    String dammage;
+    int damage;
+    int health;
+    int weight;
+    int dodge;
+
+    Fighther(String name, int damage, int health, int weight, int dodge){
+
+        this.name = name;
+        this.damage = damage;
+        this.health = health;
+        this.weight = weight;
+
+        if (dodge >= 0 && dodge <= 100) {
+            this.dodge = dodge;
+        } else {
+            this.dodge = 0;
+        }
+
+
+    }
+
+    int hit(Fighther foe) {
+        System.out.println(this.name + " => " + foe.name + " " + this.damage + " hasar vurdu.");
+
+        if (foe.isDodge()){
+            System.out.println(foe.name + " gelen hasarı blokladı");
+            System.out.println("-------");
+            return foe.health;
+        }
+        if (foe.health - this.damage < 0 ){
+            return 0;
+        }
+
+        return foe.health - this.damage;
+    }
+
+    boolean isDodge(){
+        double randomNumber = Math.random() * 100;
+        return randomNumber <= this.dodge;
+    }
+
+
+
 
 }
--- week2/Boks_Maci/src/Main.java
+++ week2/Boks_Maci/src/Main.java
@@ -1,6 +1,10 @@
 package week2.Boks_Maci.src;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("a");
+        Fighther f1 = new Fighther("A",10,120,90, 10);
+        Fighther f2 = new Fighther("B",15,85,85, 10);
+
+        Match match = new Match(f1,f2,85,90);
+        match.run();
     }
 }
