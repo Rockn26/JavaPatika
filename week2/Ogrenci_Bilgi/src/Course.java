3b31e412806535765cdc986e517c0a5a68d02afb
Rockn26 <irfanank26@gmail.com>
2023-09-20 11:17:13 +0300

    push
    push

---
+++ week2/Ogrenci_Bilgi/src/Course.java
@@ -0,0 +1,9 @@
+package week2.Ogrenci_Bilgi.src;
+
+public class Course {
+    Teacher teacher;
+    String name;
+    String code;
+    String prefix;
+    int note;
+}
--- week2/Ogrenci_Bilgi/src/Main.java
+++ week2/Ogrenci_Bilgi/src/Main.java
@@ -1,19 +1,10 @@
 package week2.Ogrenci_Bilgi.src;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Teacher t1 = new Teacher("Mahmut Hoca", "TRH", "9050000000");
+        t1.print();
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
     }
 }
