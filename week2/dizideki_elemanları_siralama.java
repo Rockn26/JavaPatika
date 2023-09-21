bf2d4aa6bf1cae33521d1a03026beac0f85cd88b
Rockn26 <irfanank26@gmail.com>
2023-09-21 16:56:11 +0300

    Dizilerdeki Elemanları sıralama
    Kullanıcan girilen dizileri küçükten büyüğe sıralama

---
+++ week2/dizideki_elemanları_siralama.java
@@ -0,0 +1,22 @@
+package week2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class dizideki_elemanları_siralama {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Dizinin boyutu : ");
+        int n = inp.nextInt();
+        int[] arrays = new int[n];
+
+        System.out.println("Dizinin elemanlarını giriniz : ");
+
+        for (int i = 0; i < arrays.length; i++) {
+            System.out.print((i+1) + " . Elemanı : ");
+            arrays[i] = inp.nextInt();
+        }
+        Arrays.sort(arrays);
+        System.out.println(Arrays.toString(arrays));
+    }
+}
