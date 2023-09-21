f6bed7c251784507e687bf2025efddb4ed8cd6ea
Rockn26 <irfanank26@gmail.com>
2023-09-21 12:39:56 +0300

    Harmonik Ortalama
    Harmonik ortalama

---
+++ week2/dizideki_eleman_ort.java
@@ -0,0 +1,23 @@
+package week2;
+
+public class dizideki_eleman_ort {
+    public static void main(String[] args) {
+        
+        int[] numbers = {1,2,3,4,5,6};
+
+        /*
+      double result = 0.0;
+        for (int number : numbers) {
+            result += (double) 1 / number;
+        }
+         */
+
+        double result = 0.0;
+        for (int i = 0; i < numbers.length; i++) {
+            result+= (double) 1 / numbers[i];
+        }
+
+        System.out.println(numbers.length / result);
+
+    }
+}
