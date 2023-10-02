8733ffe312bfbbf20fcdfc0720da0eb5862a04df
Rockn26 <irfanank26@gmail.com>
2023-10-02 19:52:38 +0300

    .
    .

--- week4/Kalitim2/src/Patika.java
+++ week4/Kalitim2/src/Patika.java
@@ -4,7 +4,9 @@ public class Patika {
     public static void main(String[] args) {
 
         Calisan c1 = new Calisan("İrfan Anık", "0555","irfan@gmail.com");
-        System.out.println(c1.getAdSoyad());
+
+        Akademisyen a1 = new Akademisyen("abc", " 0555", "ir@gmail.com", "CENG", "DOÇENT");
+
 
 
     }
