2a886e0ac2f965e38807672f0f041e431ff7f702
Rockn26 <irfanank26@gmail.com>
2023-10-02 19:29:08 +0300

    .
    .

---
+++ week4/Kalitim2/src/Calisan.java
@@ -0,0 +1,37 @@
+package week4.Kalitim2.src;
+
+public class Calisan {
+    private String adSoyad;
+    private String telefon;
+    private String eposta;
+
+    public Calisan(String adSoyad, String telefon, String eposta) {
+        this.adSoyad = adSoyad;
+        this.telefon = telefon;
+        this.eposta = eposta;
+    }
+
+    public String getAdSoyad() {
+        return adSoyad;
+    }
+
+    public void setAdSoyad(String adSoyad) {
+        this.adSoyad = adSoyad;
+    }
+
+    public String getTelefon() {
+        return telefon;
+    }
+
+    public void setTelefon(String telefon) {
+        this.telefon = telefon;
+    }
+
+    public String getEposta() {
+        return eposta;
+    }
+
+    public void setEposta(String eposta) {
+        this.eposta = eposta;
+    }
+}
