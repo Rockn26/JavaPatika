0c632e2e321d4dda1f33b6de5722e5ad42206d85
Rockn26 <irfanank26@gmail.com>
2023-10-03 13:33:34 +0300

    .
    .

--- week4/Macera_Oyunu/src/Game.java
+++ week4/Macera_Oyunu/src/Game.java
@@ -1,7 +1,15 @@
 package week4.Macera_Oyunu.src;
 
+import java.util.Scanner;
+
 public class Game {
-    public static void main(String[] args) {
-        System.out.println("a");
+
+    private Scanner inp = new Scanner(System.in);
+
+    public void start(){
+        System.out.println("Macera oyununa hoşgeldiniz!");
+        System.out.print("Lütfen bir isim giriniz : ");
+        String playerName = inp.nextLine();
     }
+
 }
