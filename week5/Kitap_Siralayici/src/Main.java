080ac75a8741f0c99d2c19e7e195f5c165d652e3
Rockn26 <irfanank26@gmail.com>
2023-10-10 16:26:37 +0300

    Kitap Sıralayıcı
    Kitap sıralayıcı ödevinin yorum satırları eklendi.

--- week5/Kitap_Siralayici/src/Main.java
+++ week5/Kitap_Siralayici/src/Main.java
@@ -5,6 +5,7 @@ import java.util.TreeSet;
 public class Main {
     public static void main(String[] args) {
 
+        // ilk sıralamamızı books da yaptık.
         TreeSet<Book> books = new TreeSet<>();
 
         Book b1 = new Book("Philosopher's Stone", "J.K. Rowling", "1997", 223);
@@ -13,12 +14,14 @@ public class Main {
         Book b4 = new Book("Goblet of Fire", "J.K. Rowling", "2000", 636);
         Book b5 = new Book("Order of the Phoenix", "J.K. Rowling", "2003", 766);
 
+        // ekleme işlemleri
         books.add(b1);
         books.add(b2);
         books.add(b3);
         books.add(b4);
         books.add(b5);
 
+        // bastırma işlemleri
         for (Book book : books) {
             System.out.println("Book name : " + book.getName() +
                     "\nAuthor name : " + book.getAuthorName() +
@@ -28,6 +31,8 @@ public class Main {
         }
         System.out.println();
 
+
+        // sayfa no ya göre sıralamayı bu sınıftan yapıyoruz :  OrderNumberComparator
         TreeSet<Book> books2 = new TreeSet<>(new OrderNumberComparator());
 
         books2.add(b1);
